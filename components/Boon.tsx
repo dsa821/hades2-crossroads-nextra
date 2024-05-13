804629708a9c918ff182abb8ce84cfe734a856a1
dsa821 <dsa821@gmail.com>
2024-05-13 11:42:33 -0700

    use Boon component on Hera's page

--- components/Boon.tsx
+++ components/Boon.tsx
@@ -9,7 +9,7 @@ type BoonProps = {
 
 export const Boon: FC<BoonProps> = (props) => {
   return (
-    <div className="bg-slate-800 text-white rounded p-4">
+    <div className={`text-lg bg-slate-800 text-white rounded p-4 ${props.className}`}>
       {props.title != null && (
         <div className='font-bold text-2xl'>
           <TextHighlight type={props.rarity}>{props.title}</TextHighlight>
