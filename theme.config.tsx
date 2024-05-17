851761403ca43e553c3619a721f6c715f8719f1b
dsa <dsa821@users.noreply.github.com>
2024-05-17 14:19:59 -0700

    feat: jump linking (#6)
    * add linking in builds, change some jump links in boons
    
    * add jump links for duo requirements
    
    * add header logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>Hades 2 - The Crossroads</span>,
+  logo: (
+    <Image
+      src="/images/hades2-logo.png"
+      alt="Hades 2 Logo"
+      height={60}
+      width={120}
+    ></Image>
+  ),
+  logoLink: true,
   head: (
     <>
-      <meta property='og:title' content='Hades 2 - The Crossroads' />
+      <meta property="og:title" content="Hades 2 - The Crossroads" />
       <meta
-        property='og:description'
-        content='A general resource for Hades 2.'
+        property="og:description"
+        content="A general resource for Hades 2."
       />
     </>
   ),
+
   primaryHue: 156.2,
   primarySaturation: 72.6,
+  darkMode: false,
+  nextThemes: {
+    forcedTheme: "dark",
+  },
+
   // project: {
   //   link: 'https://github.com/shuding/nextra-docs-template',
   // },
@@ -23,7 +38,7 @@ const config: DocsThemeConfig = {
   },
   // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    component: null,
+    text: <span className="italic">So mote it be...</span>,
   },
   editLink: {
     component: null,
