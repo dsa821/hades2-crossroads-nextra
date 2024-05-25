0090be6004cb70b6e2b78a1a24c8f42e54860bfd
dsa821 <dsa821@gmail.com>
2024-05-24 20:41:10 -0700

    add duo db stuff

--- db/boons/duos.db.ts
+++ db/boons/duos.db.ts
@@ -8,9 +8,65 @@ export const DbBoonsDuos: Record<string, BoonData> = {
     type: "duo",
   },
 
+  beachBall: {
+    name: "Beach Ball",
+    key: "beach-ball",
+    god: "duos",
+    type: "duo",
+  },
+
+  burningDesire: {
+    name: "Burning Desire",
+    key: "burning-desire",
+    god: "duos",
+    type: "duo",
+  },
+
+  chainReaction: {
+    name: "Chain Reaction",
+    key: "chain-reaction",
+    god: "duos",
+    type: "duo",
+  },
+
+  cherishedHeirloom: {
+    name: "Cherished Heirloom",
+    key: "cherished-heirloom",
+    god: "duos",
+    type: "duo",
+  },
+
+  freezerBurn: {
+    name: "Freezer Burn",
+    key: "freezer-burn",
+    god: "duos",
+    type: "duo",
+  },
+
+  funeralPyre: {
+    name: "Funeral Pyre",
+    key: "funeral-pyre",
+    god: "duos",
+    type: "duo",
+  },
+
   gloriousDisaster: {
     name: "Glorious Disaster",
     key: "glorious-disaster",
+    god: "duos",
+    type: "duo",
+  },
+
+  goldenRule: {
+    name: "Golden Rule",
+    key: "golden-rule",
+    god: "duos",
+    type: "duo",
+  },
+
+  heartyAppetite: {
+    name: "Hearty Appetite",
+    key: "hearty-appetite",
     god: "duos",
     type: "duo",
   },
@@ -50,6 +106,13 @@ export const DbBoonsDuos: Record<string, BoonData> = {
     type: "duo",
   },
 
+  phoenixSkin: {
+    name: "Phoenix Skin",
+    key: "phoenix-skin",
+    god: "duos",
+    type: "duo",
+  },
+
   queensRansom: {
     name: "Queen's Ransom",
     key: "queens-ransom",
@@ -60,6 +123,55 @@ export const DbBoonsDuos: Record<string, BoonData> = {
   romanticSpark: {
     name: "Romantic Spark",
     key: "romantic-spark",
+    god: "duos",
+    type: "duo",
+  },
+
+  roomTemperature: {
+    name: "Room Temperature",
+    key: "room-temperature",
+    god: "duos",
+    type: "duo",
+  },
+
+  scaldingVapor: {
+    name: "Scalding Vapor",
+    key: "scalding-vapor",
+    god: "duos",
+    type: "duo",
+  },
+
+  seismicHammer: {
+    name: "Seismic Hammer",
+    key: "seismic-hammer",
+    god: "duos",
+    type: "duo",
+  },
+
+  softCaress: {
+    name: "Soft Caress",
+    key: "soft-caress",
+    god: "duos",
+    type: "duo",
+  },
+
+  soulMate: {
+    name: "Soul Mate",
+    key: "soul-mate",
+    god: "duos",
+    type: "duo",
+  },
+
+  spitefulStrength: {
+    name: "Spiteful Strength",
+    key: "spiteful-strength",
+    god: "duos",
+    type: "duo",
+  },
+
+  stellarSlam: {
+    name: "Stellar Slam",
+    key: "stellar-slam",
     god: "duos",
     type: "duo",
   },
@@ -81,6 +193,13 @@ export const DbBoonsDuos: Record<string, BoonData> = {
   thermalDynamics: {
     name: "Thermal Dynamics",
     key: "thermal-dynamics",
+    god: "duos",
+    type: "duo",
+  },
+
+  torrentialDownpour: {
+    name: "Torrential Downpour",
+    key: "torrential-downpour",
     god: "duos",
     type: "duo",
   },
